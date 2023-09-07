02e403705b7924bdf8f2acb0e798b5306f40cf6d
jiho <hocube159@gmail.com>
2023-09-07 12:09:37 +0900

    SpringBoot MVC First

---
+++ src/main/java/com/ict/edu4/Edu4Application.java
@@ -0,0 +1,18 @@
+package com.ict.edu4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootConfiguration
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
+@ComponentScan(basePackages = {"com.ict.edu4.*"})
+public class Edu4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Edu4Application.class, args);
+	}
+
+}
